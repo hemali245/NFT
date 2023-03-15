e508cb5aa305e8cf440105d31f89ef12f64146f9
Hemali Pande <66758732+hemali245@users.noreply.github.com>
2023-03-15 00:45:40 +0000

    initial commit

---
+++ deploy.js
@@ -0,0 +1,15 @@
+async function main() {
+  const MyNFT = await ethers.getContractFactory("MyNFT");
+
+  // Start deployment, returning a promise that resolves to a contract object
+  const myNFT = await MyNFT.deploy();
+  console.log("Contract deployed to address:", myNFT.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
+//0xf6207dd843F87403281AC83e84F0B1B325C57b4F
